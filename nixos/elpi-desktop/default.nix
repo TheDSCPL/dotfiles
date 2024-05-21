81cbc317cf79ae67016ef4dc9b2c6d0b9bed1962
TheDSCPL <kpvrzlzzx@mozmail.com>
2024-05-21 20:22:26 +0000

    !WIP

--- nixos/elpi-desktop/default.nix
+++ nixos/elpi-desktop/default.nix
@@ -9,8 +9,8 @@ let gnome-mutter-triple-buffering =
             src = pkgs.fetchgit {
               # https://gitlab.gnome.org/vanvugt/mutter/-/commits/triple-buffering-v4-46
               url = "https://gitlab.gnome.org/vanvugt/mutter.git";
-              rev = "663f19bc02c1b4e3d1a67b4ad72d644f9b9d6970";
-              ref = "triple-buffering-v4-46";
+              #rev = "663f19bc02c1b4e3d1a67b4ad72d644f9b9d6970";
+              rev = "triple-buffering-v4-46";
               sha256 = "sha256-I1s4yz5JEWJY65g+dgprchwZuPGP9djgYXrMMxDQGrs=";         
             };
           } );
