d3357baa0a97a79796b053e91c14a9a13b7854ab
TheDSCPL <kpvrzlzzx@mozmail.com>
2024-05-21 20:10:36 +0000

    !WIP

--- nixos/elpi-desktop/default.nix
+++ nixos/elpi-desktop/default.nix
@@ -1,4 +1,24 @@
-{ nixpkgs, ... }@inputs: nixpkgs.lib.nixosSystem {
+{ nixpkgs, ... }@inputs:
+let gnome-mutter-triple-buffering =
+  { pkgs, ... }: {
+    nixpkgs.overlays = [
+      # GNOME 46: triple-buffering-v4-46
+      (final: prev: {
+        gnome = prev.gnome.overrideScope (gnomeFinal: gnomePrev: {
+          mutter = gnomePrev.mutter.overrideAttrs ( old: {
+            src = pkgs.fetchgit {
+              # https://gitlab.gnome.org/vanvugt/mutter/-/commits/triple-buffering-v4-46
+              url = "https://gitlab.gnome.org/vanvugt/mutter.git";
+              rev = "663f19bc02c1b4e3d1a67b4ad72d644f9b9d6970";
+              ref = "triple-buffering-v4-46";
+              sha256 = "sha256-I1s4yz5JEWJY65g+dgprchwZuPGP9djgYXrMMxDQGrs=";         
+            };
+          } );
+        });
+      })
+    ];
+  };
+in nixpkgs.lib.nixosSystem {
   system = "x86_64-linux";
   modules = [
     # Module knobs
@@ -12,7 +32,7 @@
         };
       };
     })
-    # ./overlays/gnome-mutter-triple-buffering
+    gnome-mutter-triple-buffering
     ./module
   ];
   specialArgs = {
