1b3bf30f3edc7c78102b51e0d9c42fe6b92a0370
TheDSCPL <kpvrzlzzx@mozmail.com>
2024-05-21 20:39:27 +0000

    !WIP

--- nixos/elpi-desktop/module.nix
+++ nixos/elpi-desktop/module.nix
@@ -32,7 +32,7 @@ in
   ];
   config = let keyboardLayout = "pt"; in {
     # Linux Kernel
-    boot.kernelPackages = builtins.traceVerbose pkgs.linuxPackages_latest.nvidiaPackages pkgs.linuxPackages_latest;
+    boot.kernelPackages = builtins.abort pkgs.linuxPackages_latest.nvidiaPackages;
 
     # Nix configurations
     system.stateVersion = "23.11";
