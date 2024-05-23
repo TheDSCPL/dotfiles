6ea254c88ff94c9caaf5b13e9c9e8169677d849c
TheDSCPL <kpvrzlzzx@mozmail.com>
2024-05-23 18:19:29 +0000

    !WIP

--- flake.nix
+++ flake.nix
@@ -10,12 +10,23 @@
   let
     nixpkgs-hyb = nixpkgs // {
       overlays = (nixpkgs.overlays or []) ++ [
-        (final: prev: {
+        /* (final: prev: {
           grilo-plugins = prev.grilo-plugins // {
             broken = true;
             replacement = nixpkgs-stable.grilo-plugins;
           };
-        })
+        }) */
+        (
+          final: prev: {
+            gom = prev.gom.overrideAttrs (oldAttrs: let version = "0.5.1"; in {
+              inherit version;
+              src = builtins.fetchurl {
+                url = "mirror://gnome/sources/${oldAttrs.pname}/${self.lib.versions.majorMinor version}/${oldAttrs.pname}-${version}.tar.xz";
+                sha256 = "sha256-15dc4d1302f8210cf08ccba9b2696417fd946d9911bbaf5183cbe36d25d570e0";
+              };
+            });
+          }
+        )
       ];
     };
   in {
