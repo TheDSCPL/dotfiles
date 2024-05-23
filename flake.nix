7b4f5f2d6b0ba9218ec55466e90a77bf9ea0b252
TheDSCPL <kpvrzlzzx@mozmail.com>
2024-05-23 19:24:35 +0000

    !WIP

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-23.11";
+    # nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-23.11";
   };
 
   outputs = args: {
