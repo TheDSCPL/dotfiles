ce528b7a65832b3f391860df08b148c52eaa7674
TheDSCPL <kpvrzlzzx@mozmail.com>
2024-05-23 13:49:12 +0000

    !WIP

--- flake.nix
+++ flake.nix
@@ -2,10 +2,25 @@
   description = "My desktop configuration";
 
   inputs = {
-    nixpkgs.url = "path:./flakeModules/nixpkgs-hybrid/";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-23.11";
   };
 
-  outputs = { self, ... }@inputs: {
-    nixosConfigurations.elpi-desktop = import ./nixos/elpi-desktop inputs;
+  outputs = { self, nixpkgs, nixpkgs-stable, ... }@args:
+  let
+    nixpkgs-hyb = nixpkgs // {
+      overlays = (nixpkgs.overlays or []) ++ [
+        (final: prev: {
+          grilo-plugins = prev.grilo-plugins // {
+            broken = true;
+            replacement = nixpkgs-stable.grilo-plugins;
+          };
+        })
+      ];
+    };
+  in {
+    nixosConfigurations.elpi-desktop = import ./nixos/elpi-desktop (
+      args // { nixpkgs = nixpkgs-hyb; }
+    );
   };
 }
