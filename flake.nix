33aab1263cd226c97147495dd54ca65f0c296812
TheDSCPL <kpvrzlzzx@mozmail.com>
2024-05-23 13:38:49 +0000

    !WIP

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My desktop configuration";
 
   inputs = {
-    nixpkgs.url = "path:./flakeModules/nixpkgs-hybrid";
+    nixpkgs.url = "path:./flakeModules/nixpkgs-hybrid/";
   };
 
   outputs = { self, ... }@inputs: {
