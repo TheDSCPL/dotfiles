ab2749df707f0ea111486735f0447494a3f66493
TheDSCPL <kpvrzlzzx@mozmail.com>
2024-05-26 23:22:42 +0000

    !WIP

--- nixos/elpi-desktop/nixpkgs-config.nix
+++ nixos/elpi-desktop/nixpkgs-config.nix
@@ -57,6 +57,16 @@
         buildInputs = oldAttrs.buildInputs ++ [ pkgs.libGL ];
       });
     })
+    (final: prev: {
+      glib = prev.glib.overrideAttrs (oldAttrs: {
+        postPatch = ''
+          ${oldAttrs.postPatch or ""}
+          # Replace all strings with pattern "^static inline" with "static __inline" in all .c and .h files
+          find . -type f -name "*.c" -exec sed -i 's/^static inline/static __inline/g' {} +
+          find . -type f -name "*.h" -exec sed -i 's/^static inline/static __inline/g' {} +
+        '';
+      });
+    })
     /* (final: prev: {
       cairo = prev.cairo.override {
         x11Support = false;
