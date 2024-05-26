02a11d344a9ecfe2450255b9f1fa807cdf891109
TheDSCPL <kpvrzlzzx@mozmail.com>
2024-05-26 15:54:26 +0000

    !WIP

--- nixos/elpi-desktop/nixpkgs-config.nix
+++ nixos/elpi-desktop/nixpkgs-config.nix
@@ -3,7 +3,7 @@
     allowUnfree = true;
     # firefox.enableAdobeFlash = true;
   };
-  /* nixpkgs.overlays = [
+  nixpkgs.overlays = [
     (final: prev: {
       # Update wayland to 1.22.93
       wayland = prev.wayland.overrideAttrs (oldAttrs: let version = "1.22.93"; in {
@@ -12,18 +12,17 @@
           url = "https://gitlab.freedesktop.org/wayland/wayland/-/releases/${version}/downloads/${oldAttrs.pname}-${version}.tar.xz";
           sha256 = "sha256-siqTGEz0oetTBFPVYLxoaqa2lUWrafuoatpEeUEDOAg=";
         };
-        patches = [];
       });
-      # Update gom to 0.5.1
+      /* # Update gom to 0.5.1
       gom = prev.gom.overrideAttrs (oldAttrs: let version = "0.5.1"; in {
         inherit version;
         src = builtins.fetchurl {
           url = "mirror://gnome/sources/${oldAttrs.pname}/${nixpkgs.lib.versions.majorMinor version}/${oldAttrs.pname}-${version}.tar.xz";
           sha256 = "sha256-FdxNEwL4IQzwjMupsmlkF/2UbZkRu69Rg8vjbSXVcOA=";
         };
-      });
+      }); */
     })
-    # GNOME 46: triple-buffering-v4-46
+    /* # GNOME 46: triple-buffering-v4-46
     (final: prev: {
       gnome = prev.gnome.overrideScope (gnomeFinal: gnomePrev: {
         mutter = gnomePrev.mutter.overrideAttrs ( old: {
@@ -36,6 +35,6 @@
           };
         });
       });
-    })
-  ]; */
+    }) */
+  ];
 }
