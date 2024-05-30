1dd432338e85487e2903df155c2bd87ae5640191
TheDSCPL <kpvrzlzzx@mozmail.com>
2024-05-30 17:35:19 +0000

    !WIP

--- nixos/elpi-desktop/default.nix
+++ nixos/elpi-desktop/default.nix
@@ -1,6 +1,8 @@
 { nixpkgs, ... }@args:
 let
   system = "x86_64-linux";
+  nixpkgs-unstable = args.nixpkgs-unstable;
+  unstable = import nixpkgs-unstable { inherit system; config = { allowUnfree = true; }; };
 in nixpkgs.lib.nixosSystem {
   inherit system;
   modules = [
@@ -16,9 +18,19 @@ in nixpkgs.lib.nixosSystem {
       };
     })
     #./nixpkgs-config.nix
+    ({pkgs, lib, config, ...}:
+    let
+      nvidia_x11 = unstable.linuxPackages.nvidiaPackages.production;
+      # nvidia-open = unstable.nvidia-open.override {
+      #   inherit (pkgs) stdenv lib fetchFromGitHub kernel;
+      #   inherit nvidia_x11;
+      # };
+    in {
+      hardware.nvidia.package = nvidia_x11;
+    })
     ./module.nix
   ];
   specialArgs = {
-    inherit args system;
+    inherit args system unstable;
   };
 }
