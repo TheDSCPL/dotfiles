13be505f5cfd9a5665320258994e02a6dfd88432
TheDSCPL <kpvrzlzzx@mozmail.com>
2024-05-30 22:09:18 +0000

    !WIP

--- nixos/elpi-desktop/configuration.nix
+++ nixos/elpi-desktop/configuration.nix
@@ -99,8 +99,6 @@ let
     # Enable the NetworkManager service to configure network connections.
     networking.networkmanager.enable = true;
     #networking.networkmanager.wifi.backend = "iwd";
-    # Enable wireless support via wpa_supplicant.
-    networking.wireless.enable = true;
     networking.hostName = hostConsts.hostname;
     # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
     # (the default) this is the recommended approach. When using systemd-networkd it's
