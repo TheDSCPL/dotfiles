980e5a88322fff27a3c3f6b3fca7d7df2f4d8f66
TheDSCPL <kpvrzlzzx@mozmail.com>
2024-06-02 13:40:40 +0000

    !WIP

--- nixos/elpi-desktop/configuration.nix
+++ nixos/elpi-desktop/configuration.nix
@@ -317,7 +317,7 @@ let
     # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
     hardware.bluetooth = {
-      enable = false;
+      enable = true;
       powerOnBoot = true;
       settings = {
         General = {
