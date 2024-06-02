8c847ab4f792fc6c7913c840e19d288fc8c48c0a
TheDSCPL <kpvrzlzzx@mozmail.com>
2024-06-02 13:42:03 +0000

    !WIP

--- nixos/elpi-desktop/default.nix
+++ nixos/elpi-desktop/default.nix
@@ -6,7 +6,8 @@ nixpkgs.lib.nixosSystem {
       config.hostConsts = {
         hostname       = "elpi-desktop";
         timezone       = "Atlantic/Azores";
-        locale         = "pt_PT.UTF-8";
+        #locale         = "pt_PT.UTF-8";
+        locale         = "en_US.UTF-8";
         keyboardLayout = "pt";
         user = {
           name     = "Elpi";
